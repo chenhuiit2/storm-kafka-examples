ddcd1e707012add613a72e2c0a66b895464e7dcd
cookie <cookiechen@ebates.com>
2017-11-23 16:30:53 +0800

    init again

---
+++ src/main/java/biz/click/Click.java
@@ -0,0 +1,25 @@
+package biz.click;
+
+public class Click {
+	private int merchantID;
+	private int userID;
+	private String clickID;
+	public int getMerchantID() {
+		return merchantID;
+	}
+	public void setMerchantID(int merchantID) {
+		this.merchantID = merchantID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getClickID() {
+		return clickID;
+	}
+	public void setClickID(String clickID) {
+		this.clickID = clickID;
+	}
+}
