2026a5c0c85d98862ebd02d9b6f40066b5c2cec3
cookie <cookiechen@ebates.com>
2017-11-29 17:47:24 +0800

    init

---
+++ src/main/java/rolling/EbatesKafkaClickSpout.java
@@ -0,0 +1,32 @@
+package rolling;
+
+import java.util.Map;
+
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+
+public class EbatesKafkaClickSpout extends BaseRichSpout {
+
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextTuple() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
